14b476d1691c07b2a64d5e9739ddb46ccdb27308
JuanKlein <juanklein@hotmail.com>
2021-12-26 17:51:34 -0300

    lo ultimo lo ultimo

--- posts/models.py
+++ posts/models.py
@@ -17,7 +17,7 @@ class Categoria(models.Model):
 
 class Post(models.Model):
 	titulo=models.CharField(max_length=50)
-	contenido=models.CharField(max_length=50)
+	contenido=models.TextField()
 	imagen=models.ImageField(upload_to="posts", null=True, blank=True)
 	autor=models.ForeignKey(User, on_delete=models.CASCADE)
 	catergorias=models.ManyToManyField(Categoria)
